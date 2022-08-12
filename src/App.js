7986059632c938b56f478143c1f0af0d2a07cf9c
“Miranda4¨ <miguel.pmiranda@alumnos.udg.mx>
2022-08-12 09:20:51 -0500

    fullCalendar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+// import Calendar from "react-calendar";
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+const localizer = momentLocalizer(moment)
+
+
 
 function App() {
+  const myEventsList = [
+    {
+      id: 0,
+      title: "All Day Event very long title",
+      allDay: true,
+      start: new Date(2015, 3, 0),
+      end: new Date(2015, 3, 1)
+    },
+    {
+      id: 1,
+      title: "Long Event",
+      start: new Date(2015, 3, 7),
+      end: new Date(2015, 3, 10)
+    },
+  
+    {
+      id: 2,
+      title: "DTS STARTS",
+      start: new Date(2016, 2, 13, 0, 0, 0),
+      end: new Date(2016, 2, 20, 0, 0, 0)
+    }
+];
   return (
     <div className='App'>
-
+      <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
     </div>
   );
 }
